9003302a57310c76bd2f4662e8c2f4b8aafc5553
Mauricio Degregori <degregori2@gmail.com>
2019-11-14 23:06:35 -0600

    mvp try2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
-
+//test
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
